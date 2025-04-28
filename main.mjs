2692e8a773d5158e0df06de6b041ec1e7e24a315
Francois Daoust <fd@tidoust.net>
2025-04-28 17:00:49 +0200

    Add analysis code and result of first analysis

---
+++ main.mjs
@@ -0,0 +1,190 @@
+import webref from '@webref/css';
+import mdn from 'mdn-data';
+import { writeFile } from 'node:fs/promises';
+import webrefPackage from '@webref/css/package.json' with { type: 'json' };
+import mdnPackage from 'mdn-data/package.json' with { type: 'json' };
+
+/**
+ * MDN data wants to list CSS units. They are not defined as such in CSS specs
+ * but can be easily computed by looking at the list of values that are defined
+ * for the restricted set of CSS types defined here.
+ */
+const unitTypes = [
+  '<angle>',
+  '<flex>',
+  '<length>',
+  '<frequency>',
+  '<resolution>',
+  '<time>'
+];
+
+/**
+ * Webref data is per spec. Goal is to create the same view as in MDN data.
+ */
+const categorized = {
+  atRules: [],
+  functions: [],
+  properties: [],
+  selectors: [],
+  syntaxes: [],
+  types: [],
+  units: []
+};
+
+
+/************************************************************
+ * Categorize Webref data following the same structure as
+ * MDN data
+ ***********************************************************/
+const parsedFiles = await webref.listAll();
+for (const [shortname, data] of Object.entries(parsedFiles)) {
+  // Same categorization in Webref and MDN data for at-rules, properties,
+  // and selectors.
+  categorized.atRules.push(...(data.atrules ?? []));
+  categorized.properties.push(...(data.properties ?? []));
+  categorized.selectors.push(...(data.selectors ?? []));
+
+  // Then Webref data has a mix bax of values, some of them being root
+  // values, while others are scoped to some other construct, whereas MDN data
+  // separates things into functions, syntaxes, types, and units (and does not
+  // seem to have the notion of scoping.
+  // Definitions in CSS specs do (badly from time to time, but so be it)
+  // distinguish between functions and types. Let's use that information
+  let values = data.values ?? [];
+  categorized.functions.push(...values.filter(v => v.type === 'function'));
+  categorized.types.push(...values.filter(v => v.type === 'type'));
+
+  // ... and add scoped functions since MDN data treat them similarly to
+  // non-scoped ones.
+  for (const property of data.properties ?? []) {
+    categorized.functions.push(...(property.values ?? [])
+      .filter(v => v.type === 'function'));
+  }
+  for (const val of data.values ?? []) {
+    categorized.functions.push(...(val.values ?? [])
+      .filter(v => v.type === 'function'));
+  }
+}
+
+// I'm not fully clear what syntaxes are supposed to be.
+// They seem to be the union of functions and types.
+categorized.syntaxes.push(...categorized.functions);
+categorized.syntaxes.push(...categorized.types);
+
+// Units are not defined as such in CSS, but they are the possible values of a
+// restricted number of types in practice.
+for (const type of categorized.types) {
+  if (unitTypes.includes(type.name)) {
+    categorized.units.push(...type.values);
+  }
+}
+
+
+/************************************************************
+ * Identify gaps
+ ***********************************************************/
+const missing = {
+  webref: {},
+  mdn: {}
+};
+
+for (const category of Object.keys(categorized)) {
+  missing.webref[category] = [];
+  missing.mdn[category] = [];
+}
+
+for (const category of Object.keys(categorized)) {
+  for (const feature of categorized[category]) {
+    const featureName = feature.name.match(/^<(.+?)>$/) ?
+      feature.name.replace(/^<(.+?)>$/, '$1') :
+      feature.name;
+    if (!mdn.css[category][featureName]) {
+      missing.mdn[category].push({
+        name: feature.name,
+        href: feature.href
+      });
+    }
+  }
+}
+
+for (const category of Object.keys(categorized)) {
+  for (const featureName of Object.keys(mdn.css[category])) {
+    if (!categorized[category].find(feature =>
+        feature.name === featureName ||
+        feature.name === '<' + featureName + '>')) {
+      missing.webref[category].push({
+        name: featureName,
+        href: mdn.css[category][featureName].mdn_url
+      });
+    }
+  }
+}
+
+
+/************************************************************
+ * Save categorized Webref data
+ ***********************************************************/
+await writeFile(
+  'webref.json',
+  JSON.stringify(categorized, null, 2),
+  'utf8'
+);
+
+
+/************************************************************
+ * Write gap report
+ ***********************************************************/
+let report = ['# Gap analysis between MDN data and Webref'];
+report.push('');
+report.push(`Generated on **${new Date().toISOString()}** using **v${mdnPackage.version}** of MDN data and **v${webrefPackage.version}** of \`@webref/css\`.`);
+report.push('');
+for (const source of ['webref', 'mdn']) {
+  report.push(`### Missing from ${source}`);
+  for (const category of Object.keys(categorized)) {
+    const nb = missing[source][category].length;
+    if (nb > 0) {
+      report.push(`
+<details>
+<summary>${nb} ${getCatName(category, nb)} missing from ${source}</summary>
+
+${reportFeatures(missing[source][category])}
+</details>
+`);
+    }
+  }
+}
+
+await writeFile('report.md', report.join('\n'), 'utf8');
+
+
+/************************************************************
+ * A few helper functions to help with reporting
+ ***********************************************************/
+function reportFeatures(features) {
+  return features.map(reportFeature).join('\n');
+}
+
+function reportFeature(feature) {
+  const decoratedName = '`' + feature.name + '`';
+  if (feature.href) {
+    return `- [${decoratedName}](${feature.href})`;
+  }
+  return '- ' + decoratedName;
+}
+
+function getCatName(category, nb) {
+  const s = nb > 1 ? 's' : '';
+  if (category === 'atRules') {
+    return `at-rule${s}`;
+  }
+  if (nb > 1) {
+    return category;
+  }
+  if (category === 'properties') {
+    return `property`;
+  }
+  if (category === 'syntaxes') {
+    return `syntax`;
+  }
+  return category.substring(0, category.length-1);
+}
